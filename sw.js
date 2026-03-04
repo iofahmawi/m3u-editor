10c0e90fdcc9bec22f2a352ff9480c552a8fc887
iofahmawi <mo.fahmawi@gmail.com>
2026-03-04 05:40:08 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js - M3U Editor (Dynamic Version)
 
-const CACHE_NAME = 'm3u-editor-dynamic-v3';
+const CACHE_NAME = 'm3u-editor-dynamic-v4';
 
 // نخزن الملفات المحلية الأساسية فقط لضمان التثبيت السريع والناجح
 const urlsToCache = [
